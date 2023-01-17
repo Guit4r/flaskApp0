fd739a49bb0ba8a8feaef8bbd2a0d83e7c84125e
Guit4r <guitar230545@gmail.com>
2023-01-17 15:18:38 +0700

    Lab09: AJAX, jQuery and Bootstrap Table

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '29a8a9e70366c8c25f39781653eaf21b607c236f1db2bae6'
+app.config['JSON_AS_ASCII'] = False
  
 from app import views  # noqa
